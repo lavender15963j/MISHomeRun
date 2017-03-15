710707def2ed55330af4480a823cff56ce395bc1
Lavender <lavender15963j@gmail.com>
2017-03-15 15:56:38 +0800

    REM cmd

--- MISHomeRun/Configure.bat
+++ MISHomeRun/Configure.bat
@@ -4,9 +4,9 @@ python -m pip install --upgrade pip
 
 pip install -r REQUIREMENTS.txt
 
-python manage.py migrate --noinput
+REM python manage.py migrate --noinput
 
 REM http://gnuwin32.sourceforge.net/packages/unzip.htm is required.
 REM unzip -u initial_data.zip
 
-python manage.py loaddata superuser.json
+REM python manage.py loaddata superuser.json
